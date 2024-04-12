0687e82c2ac9949704432703459bda59cd7d487d
Castormannaro03 <federicofontana09112003@gmail.com>
2024-04-12 17:03:07 +0200

    Aggiustamenti file MainActivity.kt

--- app/src/main/java/com/example/loginapp/MainActivity.kt
+++ app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -52,28 +52,30 @@ class MainActivity : AppCompatActivity() {
         Log.v("error", email.text.toString())
         val password = findViewById<TextView>(R.id.TextPassword)
 
-        auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithEmail:success")
-                    Toast.makeText(baseContext,"Login riuscito", Toast.LENGTH_SHORT).show()
-                    val user = auth.currentUser
-                    updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        baseContext,
-                        "Authentication failed.",
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                    updateUI(null)
+        if(email.text.toString()!="" && password.text.toString()!=""){
+            auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        Toast.makeText(baseContext, "Login riuscito", Toast.LENGTH_SHORT).show()
+                        val user = auth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                        updateUI(null)
+
+                    }
 
                 }
+        }
 
-            }
-        //auth.signOut()
     }
 
     //Change UI according to user data.
@@ -81,6 +83,7 @@ class MainActivity : AppCompatActivity() {
         if (account != null) {
             Toast.makeText(this, "You Signed In successfully", Toast.LENGTH_LONG).show()
             startActivity(Intent(this, LogOut::class.java))
+            finish()
         } else {
             Toast.makeText(this, "You Didnt signed in", Toast.LENGTH_LONG).show()
         }
