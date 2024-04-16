b18a2e318298d612fdf24ae17bb882a5ebd222f2
Castoromannaro03 <federicofontana09112003@gmail.com>
2024-04-16 18:43:37 +0200

    Cambio stile pulsante Google.

--- app/src/main/java/com/example/loginapp/MainActivity.kt
+++ app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -50,7 +51,7 @@ class MainActivity : AppCompatActivity() {
             finish() // finish the current activity to prevent the user from coming back to the SignInActivity using the back button
         }
 
-        val signInButton = findViewById<Button>(R.id.googleButton)
+        val signInButton = findViewById<ImageButton>(R.id.googleButton)
         signInButton.setOnClickListener {
             signIn()
         }
