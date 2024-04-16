7b2b6007a230408ef6d4869e7ae6209587a001ac
Giacomo <playgiacomo@gmail.com>
2024-04-16 15:39:52 +0200

    Migliorata la schermata di registrazione e aggiunta pulsante indietro

--- app/src/main/java/com/example/loginapp/Registration.kt
+++ app/src/main/java/com/example/loginapp/Registration.kt
@@ -33,31 +33,41 @@ class Registration : AppCompatActivity() {
         //Toast.makeText(baseContext, "Entro", Toast.LENGTH_LONG).show()
         val email = findViewById<TextView>(R.id.RegistrationEmailAddress)
         val password = findViewById<TextView>(R.id.RegistrationPassword)
+        val emailRepeat = findViewById<TextView>(R.id.RegistrationEmailAddressRepeat)
+        val passwordRepeat = findViewById<TextView>(R.id.RegistrationPasswordRepeat)
         Log.v("errore", email.text.toString())
         //Toast.makeText(baseContext, "Entro", Toast.LENGTH_LONG).show()
 
         if(email.text.toString()!="" && password!=null){
-            Log.v("errore", "entro nel metodo")
-            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "createUserWithEmail:success")
-                        val user = auth.currentUser
-                        updateUI(user)
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                        Toast.makeText(
-                            baseContext,
-                            "Authentication failed.",
-                            Toast.LENGTH_SHORT,
-                        ).show()
-                        updateUI(null)
+            if(emailRepeat.text.toString() == email.text.toString() && passwordRepeat.text.toString() == password.text.toString()) {
+                Log.v("errore", "entro nel metodo")
+                auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success")
+                            val user = auth.currentUser
+                            updateUI(user)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                            Toast.makeText(
+                                baseContext,
+                                "Authentication failed.",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                            updateUI(null)
+                        }
                     }
-                }
+            } else {
+
+                Toast.makeText(baseContext, "I valori dell'Email o Password non corrispondono", Toast.LENGTH_SHORT).show()
+
+            }
+        } else {
+            Toast.makeText(baseContext, "Email o Password non validi", Toast.LENGTH_SHORT).show()
+            //Log.v("errore", "entro nel metodo")
         }
-        //Log.v("errore", "entro nel metodo")
 
     }
 
@@ -68,5 +78,11 @@ class Registration : AppCompatActivity() {
         } else {
             Toast.makeText(this, "You Didnt signed in", Toast.LENGTH_LONG).show()
         }
+    }
+
+    fun goBack(view: View) {
+
+        startActivity(Intent(this, MainActivity::class.java))
+
     }
 }
