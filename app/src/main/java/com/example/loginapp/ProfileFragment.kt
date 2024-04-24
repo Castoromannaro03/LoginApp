0671c0324d0114bdd15c1d67ccb4ed1c9eaa0000
Giacomo <playgiacomo@gmail.com>
2024-04-24 17:38:18 +0200

    Aggiunta ViewBinding nel ProfileFragment e query per ricavare nome utente

--- app/src/main/java/com/example/loginapp/ProfileFragment.kt
+++ app/src/main/java/com/example/loginapp/ProfileFragment.kt
@@ -1,14 +1,63 @@
 package com.example.loginapp
 
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.loginapp.databinding.ProfileFragmentBinding
 import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.firestore
+
 
 class ProfileFragment : Fragment(R.layout.profile_fragment) {
 
+    val databaseUser = Firebase.firestore
+    val User = databaseUser.collection("Utente")
+    private lateinit var viewOfLayout: View
+
+    private var _binding: ProfileFragmentBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = ProfileFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        var queryUser = User.whereEqualTo(FieldPath.documentId(), Firebase.auth.currentUser?.email.toString())
+
+        val risultato = queryUser.get().addOnSuccessListener {result ->
+
+            var nome = result.documents[0].get("Nome")
+            Log.v("Nome", nome.toString())
+            binding.textView7.text = nome.toString()
+
+        }.addOnFailureListener{
+
+            Log.v("Failure", "Ha fallito")
+
+        }
+
+
+
+        return view
     }
 
 }
