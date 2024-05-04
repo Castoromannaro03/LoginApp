eba0de6d4bfbc0cde3841eeb6ab100e6a0d8ca80
Castoromannaro03 <federicofontana09112003@gmail.com>
2024-05-04 19:21:44 +0200

    Prima prova bacheca(non funzionante)

--- app/src/main/java/com/example/InsubriApp/NoticeboardFragment.kt
+++ app/src/main/java/com/example/InsubriApp/NoticeboardFragment.kt
@@ -1,12 +1,15 @@
 package com.example.InsubriApp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.example.InsubriApp.databinding.NoticeboardFragmentBinding
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class NoticeboardFragment : Fragment(R.layout.noticeboard_fragment) {
 
@@ -16,11 +19,6 @@ class NoticeboardFragment : Fragment(R.layout.noticeboard_fragment) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-
-
-
     }
 
 
@@ -29,6 +27,30 @@ class NoticeboardFragment : Fragment(R.layout.noticeboard_fragment) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = NoticeboardFragmentBinding.inflate(inflater, container, false)
+
+
+        val bacheca = Firebase.firestore.collection("Bacheca")
+        var arrayPost = ArrayList<String>()
+
+        arrayPost.add("Ciao")
+
+
+        bacheca.get().addOnSuccessListener { result ->
+            for (item in result.documents) {
+                Log.v("ERRORONE", item.get("Titolo").toString())
+                arrayPost.add(item.get("Titolo").toString())
+            }
+
+
+
+
+
+
+        }
+            .addOnFailureListener {
+                Log.v("Firestore Bacheca", "Errore nel recupero della bacheca")
+            }
 
         val users = arrayOf(
             "Virat Kohli", "Rohit Sharma", "Steve Smith",
@@ -36,15 +58,14 @@ class NoticeboardFragment : Fragment(R.layout.noticeboard_fragment) {
             "Giuseppe verdi", "Pippo Baudo",
             "Virat Kohli", "Rohit Sharma", "Steve Smith",
             "Kane Williamson", "Ross Taylor", "Mario Rossi",
-            "Giuseppe verdi", "Pippo Baudo")
-
-        _binding = NoticeboardFragmentBinding.inflate(inflater, container, false)
-        val view = binding.root
-
+            "Giuseppe verdi", "Pippo Baudo"
+        )
 
         binding.listView.adapter =
-            this.context?.let { ArrayAdapter(it, android.R.layout.simple_list_item_1, users) }
+            this.context?.let { ArrayAdapter(it, android.R.layout.simple_list_item_1, arrayPost) }
 
+
+        val view = binding.root
 
         return view
     }
